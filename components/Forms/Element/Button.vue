721d56bb866d481e38ee0182f14182927c2f8f5c
Baerspektivo <botalinom@gmail.com>
2024-02-23 18:19:45 +0100

    startet styling button

--- components/Forms/Element/Button.vue
+++ components/Forms/Element/Button.vue
@@ -4,5 +4,5 @@ defineProps({
 });
 </script>
 <template>
-  <button :disabled="disabled" class="rounded-full"><slot /></button>
+  <button :disabled="disabled" class="rounded-full p-2 m-2"><slot /></button>
 </template>
